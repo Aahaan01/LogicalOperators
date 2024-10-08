d6d86c51b1f2898210c820ef8e87ab8391909511
Aahaan01 <160381765+Aahaan01@users.noreply.github.com>
2024-10-08 19:09:57 +0530

    Add files via upload

---
+++ andOr.py
@@ -0,0 +1,13 @@
+a=40
+b=-99
+c=17
+
+if a and b and c:
+    print("All the variables exist")
+else:
+    print("Not all variables exist")
+    
+if a>0 or b>0:
+    print("a and b are positive numbers")
+else:
+    print("Not all variables are positive")
---
+++ not.py
@@ -0,0 +1,13 @@
+a=-7
+b=0
+c=7
+
+print(a!=b, "a is not equal to b")
+print(b!=c, "b is not equal to c ")
+
+print(a==b, "a is equal to b")
+
+a="python"
+b="javaScript"
+if a!=b:
+    print(a, "and", b, "are different")
